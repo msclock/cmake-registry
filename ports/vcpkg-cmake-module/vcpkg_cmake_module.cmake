c8a9870e403638580fe4b56bedb54d7b167272cf
msclock <msclock@qq.com>
2023-10-11 03:54:12 +0800

    perf: vcpkg-cmake-module install directories
    Signed-off-by: msclock <msclock@qq.com>

--- ports/vcpkg-cmake-module/vcpkg_cmake_module.cmake
+++ ports/vcpkg-cmake-module/vcpkg_cmake_module.cmake
@@ -28,8 +28,16 @@ function(vcpkg_cmake_module)
     set(arg_MODULE_PATH "${arg_PACKAGE_NAME}")
   endif()
 
-  set(_modules_dir_path "cmake")
+  set(_modules_dir_path "_modules")
   set(_modules_path "${_modules_dir_path}/${arg_MODULE_PATH}")
+  set(_export_in_vcpkg_cmake_wrapper
+      "include_guard(GLOBAL)\n\nlist(APPEND CMAKE_MODULE_PATH \${CMAKE_CURRENT_LIST_DIR}/${_modules_dir_path})\n"
+  )
+  string(
+    APPEND
+    _export_in_vcpkg_cmake_wrapper
+    "list(APPEND CMAKE_PREFIX_PATH \${CMAKE_CURRENT_LIST_DIR}/${_modules_path})\n"
+  )
 
   if(arg_FILE_LIST)
     foreach(_file ${arg_FILE_LIST})
@@ -40,18 +48,51 @@ function(vcpkg_cmake_module)
 
   if(arg_DIRECTORY_LIST)
     foreach(_dir "${arg_DIRECTORY_LIST}")
-      install(
-        DIRECTORY "${_dir}"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/${_modules_path})
+      if(NOT IS_DIRECTORY ${_dir})
+        message(
+          FATAL_ERROR
+            "DIRECTORY_LIST must pass with directories, error: ${arg_DIRECTORY_LIST}"
+        )
+      endif()
+
+      get_filename_component(_dir_name ${_dir} NAME_WE)
+
+      string(
+        APPEND
+        _export_in_vcpkg_cmake_wrapper
+        "list(APPEND CMAKE_PREFIX_PATH \${CMAKE_CURRENT_LIST_DIR}/${_modules_path}/${_dir_name})\n"
+      )
+
+      file(
+        GLOB_RECURSE _dir_itmes
+        LIST_DIRECTORIES ON
+        RELATIVE "${_dir}"
+        "${_dir}/*")
+      list(FILTER _dir_itmes EXCLUDE REGEX [[^\.]])
+      message(STATUS ${_dir_itmes})
+      foreach(_item ${_dir_itmes})
+        if(IS_DIRECTORY "${_dir}/${_item}")
+          string(
+            APPEND
+            _export_in_vcpkg_cmake_wrapper
+            "${_export_in_vcpkg_cmake_wrapper}list(APPEND CMAKE_PREFIX_PATH "
+            "\${CMAKE_CURRENT_LIST_DIR}/${_modules_dir_path}/${_dir_name}/${_item})\n"
+          )
+        endif()
+      endforeach()
+
+      # Install as subdir
+      file(INSTALL ${_dir}
+           DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/${_modules_path}")
+
     endforeach()
   endif()
 
-  file(
-    WRITE ${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake
-    "include_guard(GLOBAL)\n\nlist(APPEND CMAKE_MODULE_PATH \${CMAKE_CURRENT_LIST_DIR}/${_modules_dir_path})\n"
-  )
+  file(WRITE ${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake
+       ${_export_in_vcpkg_cmake_wrapper})
 
   unset(_modules_dir_path)
   unset(_modules_path)
+  unset(_export_in_vcpkg_cmake_wrapper)
 
 endfunction()
