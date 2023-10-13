91b60d397c01049fa7a5dc6b65bb2da80c3bee51
msclock <msclock@qq.com>
2023-10-13 15:08:36 +0800

    chore(ports): [stablecoder-cmake-scripts] new port
    Signed-off-by: msclock <msclock@qq.com>

---
+++ ports/stablecoder-cmake-scripts/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  REPO
+  StableCoder/cmake-scripts
+  REF
+  8a92770d1e518a86788b67ab0e4e5f965a399cc8
+  SHA512
+  bb27c23adde569f3d2bcf5620b149381281496782072ff544c37b6bca6265a9936f1e82b41483a77d2efa3a2178cd3b9b5a12bb40e71257598e27388db4d91d6
+  HEAD_REF
+  main)
+
+file(GLOB items ${SOURCE_PATH}/*)
+list(FILTER items EXCLUDE REGEX
+     [[LICENSE|.gitlab-ci.yml|.clang-format|img|example|.gitignore]])
+vcpkg_cmake_module(PATH_LIST ${items})
+
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage
+     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/LICENSE)
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
