3b7b4380e096a59ef5d323f8194e1cac74b27ff8
msclock <msclock@users.noreply.github.com>
2024-04-06 06:38:27 +0800

    perf: exclude chores

--- ports/robotology-cmake-ycm/portfile.cmake
+++ ports/robotology-cmake-ycm/portfile.cmake
@@ -10,7 +10,8 @@ vcpkg_from_github(
 )
 
 file(GLOB items ${SOURCE_PATH}/*)
-list(FILTER items EXCLUDE REGEX [[deprecated|tests|LICENSES|.reuse|.github]])
+list(FILTER items EXCLUDE REGEX
+     "${SOURCE_PATH}/(deprecated|tests|LICENSES|\.reuse|\.github)")
 vcpkg_cmake_module(PATH_LIST ${items})
 
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage
--- ports/stablecoder-cmake-scripts/portfile.cmake
+++ ports/stablecoder-cmake-scripts/portfile.cmake
@@ -11,8 +11,13 @@ vcpkg_from_github(
   main)
 
 file(GLOB items ${SOURCE_PATH}/*)
-list(FILTER items EXCLUDE REGEX
-     [[LICENSE|.gitlab-ci.yml|.clang-format|img|example|.gitignore]])
+list(
+  FILTER
+  items
+  EXCLUDE
+  REGEX
+  "${SOURCE_PATH}/(LICENSE|\.gitlab-ci\.yml|\.clang-format|img|example|\.gitignore)"
+)
 vcpkg_cmake_module(PATH_LIST ${items})
 
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage
