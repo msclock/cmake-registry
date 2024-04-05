994c1169037588bffcb913f86261fec2e620b7c9
msclock <msclock@qq.com>
2024-04-06 06:49:27 +0800

    perf: exclude chores (#107)
    Co-authored-by: msclock <msclock@users.noreply.github.com>

--- ports/rpavlik-cmake-modules/portfile.cmake
+++ ports/rpavlik-cmake-modules/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
   main)
 
 file(GLOB items ${SOURCE_PATH}/*)
-list(FILTER items EXCLUDE REGEX [[reuse|LICENSES|module-docs]])
+list(FILTER items EXCLUDE REGEX "${SOURCE_PATH}/(reuse|LICENSES|module-docs)")
 vcpkg_cmake_module(PATH_LIST ${items})
 
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage
