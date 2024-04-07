8845bf5fbab0f1d2258d64a2bb81097b13c763b5
msclock <msclock@qq.com>
2024-04-07 11:40:14 +0800

    chore: remove changelog generation
    Signed-off-by: msclock <msclock@qq.com>

--- .releaserc.js
+++ .releaserc.js
@@ -49,14 +49,6 @@ module.exports = {
         },
       },
     ],
-    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      },
-    ],
     "@semantic-release/github",
   ],
 };
